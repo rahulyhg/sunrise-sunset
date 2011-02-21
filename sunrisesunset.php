8bb7d49fbdd2d37b19b1df5a829d767dfaec92d5
rxn <rxn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-21 07:09:55 +0000

    Added Drop Down List
    git-svn-id: https://plugins.svn.wordpress.org/sunrise-sunset/trunk@348513 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sunrisesunset.php
+++ sunrisesunset.php
@@ -32,34 +32,63 @@ function register_sunrise_sunset() {
 
 class sunrise_sunset extends WP_Widget {
 
+
     function sunrise_sunset() {
 
         $widget_ops = array('classname' => 'sunrise_sunset_widget'
-        , 'description' => __('Displays Sunrise and Sunset Times', 'ss-plugin'));
+        , 'description' => 'Displays Sunrise and Sunset Times');
 
-        $this->WP_Widget('ss_widget_bio', __('Sunrise Sunset', 'ss-plugin'), $widget_ops);
+        $this->WP_Widget('ss_widget_bio', 'Sunrise Sunset', $widget_ops);
     }
 
     function form($instance) {
-        $defaults = array('title' => __('Sunrise Sunset Times', 'ss-plugin'));
+        $defaults = array(
+            'title' => 'Sunrise Sunset Times',
+            'timezone' => 'Time Zone', 'ss-plugin');
 
         $instance = wp_parse_args((array) $instance, $defaults);
         $title = strip_tags($instance['title']);
+        $timezone = strip_tags($instance['timezone']);
 
         ?>
-        <p><?php _e('Title', 'ss-plugin') ?>:
+        <p><?php echo 'Title' ?>:
             <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>"
                    type="text" value="<?php echo esc_attr($title); ?>"/>
         </p>
+
+        <p><?php echo 'Time Zone'; ?>:
+
         <?php
 
+        $select_box = sprintf('<select name="%s">', $this->get_field_name('timezone'));
+
+        if ($instance['timezone'] == 'America/Chicago') {
+            $select_box .= sprintf('<option value="%s" selected="yes">%s</option>', 'America/Chicago', 'Chicago');
+        } else {
+            $select_box .= sprintf('<option value="%s">%s</option>', 'America/Chicago', 'Chicago');
+        }
+
+        if ($instance['timezone'] == 'America/New_York') {
+            $select_box .= sprintf('<option value="%s" selected="yes">%s</option>', 'America/New_York', 'New_York');
+        } else {
+            $select_box .= sprintf('<option value="%s">%s</option>', 'America/New_York', 'New_York');
+        }
+
+        if ($instance['timezone'] == 'America/Los_Angeles') {
+            $select_box .= sprintf('<option value="%s" selected="yes">%s</option>', 'America/Los_Angeles', 'Los_Angeles');
+        } else {
+            $select_box .= sprintf('<option value="%s">%s</option>', 'America/Los_Angeles', 'Los_Angeles');
+        }
+
+        $select_box .= '</select>';
+        echo $select_box;
     }
 
     // save our widget
     function update($new_instance, $old_instance) {
         $instance = $old_instance;
         $instance['title'] = strip_tags($new_instance['title']);
-
+        $instance['timezone'] = strip_tags($new_instance['timezone']);
         return $instance;
     }
 
@@ -77,7 +106,7 @@ class sunrise_sunset extends WP_Widget {
         }
 
 
-        $result = $this->ss_get_sunset();
+        $result = $this->ss_get_sunset($instance);
 
         echo $today . "<br/>";
         echo "Sunrise:" . $result['sunrise'] . "<br/>";
@@ -87,34 +116,33 @@ class sunrise_sunset extends WP_Widget {
     }
 
 
-    function ss_get_sunset() {
+    function ss_get_sunset($instance) {
 
-        $myTZ = 'America/Los_Angeles'; // Pacific Time
+        $latitude = array('America/Chicago' => 41.51, 'America/Los_Angeles' => 34.30, 'America/New_York' => 40.47);
+        $longitude = array('America/Chicago' => -87.39, 'America/Los_Angeles' => -118.15, 'America/New_York' => -73.58);
+
+        $target_time_zone = $instance['timezone'];
         $time_format = 'h:i A T'; // 08:53 PM PDT
-
-        // Set the latitude and longitude for your location
-        $latitude = 46.36; //North
-        $longitude = -124.02; //West
 
         $zenith = 90 + (50 / 60);
 
         // Set timezone in PHP5/PHP4 manner
         if (!function_exists('date_default_timezone_set')) {
-            putenv("TZ=" . $myTZ);
+            putenv("TZ=" . $target_time_zone);
         } else {
-            date_default_timezone_set("$myTZ");
+            date_default_timezone_set("$target_time_zone");
         }
 
         // find time offset in hours
         $tzoffset = date("Z") / 60 / 60;
 
         // determine sunrise time
-        $sunrise = date_sunrise(time(), SUNFUNCS_RET_STRING, $latitude, $longitude, $zenith, $tzoffset);
+        $sunrise = date_sunrise(time(), SUNFUNCS_RET_STRING, $latitude[$target_time_zone], $longitude[$target_time_zone], $zenith, $tzoffset);
         $sunrise_time = date($time_format, strtotime(date("Y-m-d") . ' ' . $sunrise));
 
 
         // determine sunset time
-        $sunset = date_sunset(time(), SUNFUNCS_RET_STRING, $latitude, $longitude, $zenith, $tzoffset);
+        $sunset = date_sunset(time(), SUNFUNCS_RET_STRING, $latitude[$target_time_zone], $longitude[$target_time_zone], $zenith, $tzoffset);
         $sunset_time = date($time_format, strtotime(date("Y-m-d") . ' ' . $sunset));
 
         return array('sunrise' => $sunrise_time, 'sunset' => $sunset_time);
